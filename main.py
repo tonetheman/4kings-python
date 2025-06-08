bb13d1ccb9c954a4b211049a0be55a0259af62c3
tonethemna <tonetheman@gmail.com>
2025-06-08 15:20:33 -0500

    working and readme

--- main.py
+++ main.py
@@ -51,82 +51,107 @@ class Pile:
     def pop(self):
         return self.cards.pop()
 
+def one_hand(simcount):
+    # one deck
+    d = Deck()
+    # shuffle it
+    d.shuffle()
+    # make 12 piles
+    piles = []
+    for i in range(12):
+        # print("making pile",i)
+        piles.append(Pile())
+    index = 0
+    for i in range(52-4):
+        piles[index].push(d.pop())
+        index += 1
+        if index==12:
+            index = 0
 
-# one deck
-d = Deck()
-# shuffle it
-d.shuffle()
-# make 12 piles
-piles = []
-for i in range(12):
-    print("making pile",i)
-    piles.append(Pile())
-index = 0
-for i in range(52-4):
-    piles[index].push(d.pop())
-    index += 1
-    if index==12:
-        index = 0
 
+    print("running simcount",simcount)
+    # grab the top card that is left from the deck
+    mycard = d.pop()
+    loopcount=0
+    king_count = 0
+    while True:
+        # print(loopcount,"start of loop...")
+        # print("mycard",mycard)
 
-# grab the top card that is left from the deck
-mycard = d.pop()
-loopcount=0
-king_count = 0
-while True:
-    print(loopcount,"start of loop...")
-
-    print("mycard",mycard)
-
-    if mycard.rank == KING:
-        print("mycard is a KING need to get a new card",mycard)
-        # bump up the king count
-        king_count += 1
-        # if we have them all we are done
-        if king_count==3:
-            print("king count was 3 we are done")
-            break
-        # pull a new card
-        mycard = d.pop()
-        if mycard.rank==KING:
-            print("WTF we pulled a 2nd king")
-            # bad luck we pulled a king!
+        if mycard.rank == KING:
+            # print("mycard is a KING need to get a new card",mycard)
+            # bump up the king count
             king_count += 1
+            # if we have them all we are done
             if king_count==3:
                 print("king count was 3 we are done")
                 break
             # pull a new card
             mycard = d.pop()
+            if mycard.rank==KING:
+                # print("WTF we pulled a 2nd king")
+                # bad luck we pulled a king!
+                king_count += 1
+                if king_count==3:
+                    print("king count was 3 we are done")
+                    break
+                # pull a new card
+                mycard = d.pop()
+                if mycard.rank == KING:
+                    print("WTF we pulled a 3rd king ... impossible")
+                    king_count += 1
+                    if king_count==3:
+                        print("king count was 3 we are done")
+                        break
 
-    # pile we care about
-    p = piles[mycard.rank]
-    # print("pile it should move to",p)
+        # pile we care about
+        p = piles[mycard.rank]
+        # print("pile it should move to",p)
 
-    # grab top card from the pile in question
-    newcard = p.pop()
-    # print("pile after pop",p)
-    # print("newcard",newcard)
+        # grab top card from the pile in question
+        newcard = p.pop()
+        # print("pile after pop",p)
+        # print("newcard",newcard)
 
-    # put the card 
-    p.push_bottom(mycard)
-    # print("pile after push bottom",p)
+        # put the card 
+        p.push_bottom(mycard)
+        # print("pile after push bottom",p)
 
-    # set the card for the next round
-    mycard = newcard
+        # set the card for the next round
+        mycard = newcard
 
-    loopcount+=1
+        loopcount+=1
 
-def score_piles():
-    in_place = 0
-    for i,v in enumerate(piles):
-        for j,jv in enumerate(v.cards):
-            if i==jv.rank:
-                in_place += 1
-    print("in place is",in_place)
+    def score_piles():
+        in_place = 0
+        for i,v in enumerate(piles):
+            for j,jv in enumerate(v.cards):
+                if i==jv.rank:
+                    in_place += 1
+        print("in place is",in_place)
+        return in_place
 
-for i,v in enumerate(piles):
-    print(i)
-    print(v)
+    """
+    if simcount==56:
+        for i,v in enumerate(piles):
+            print(i)
+            print(v)
 
-print("deck at end",d)
-score_piles()
+    """
+
+    # print("deck at end",d)
+    return score_piles()
+
+index = 0
+win_count = 0
+while True:
+    res = one_hand(index)
+    index += 1
+    if res==48: # this is a win!
+        win_count += 1
+    
+    if index>110000:
+        break
+print("index",index)
+print("win count",win_count)
+
---
+++ readme.md
@@ -0,0 +1,25 @@
+# four kings solitaire
+
+From my youth someone (?) taught me this card game.
+
+There is no skill it is random chance.
+
+- make 12 piles of 4
+- you will be left with 4 cards (the draw pile)
+- draw a card from the draw pile
+- loop starts here
+- find the pile that matches the rank
+    - if you drew a 4h then you will find the 4th pile
+    - place you 4h on the bottom of the pile
+    - take a card from that pile
+    - if you found a king (boooo)
+        - set the king aside
+        - get your next card from the draw pile
+        - go to start of loop
+    - if you did not find a king go to the start of the loop
+
+The idea is that you want to end up with everything sorted and the 4 kings left over.
+
+I never won that game. Not even once.
+
+I wrote a simulation to figure out why.
